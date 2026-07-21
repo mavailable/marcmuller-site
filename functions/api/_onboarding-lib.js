a40ba4c1577c78804e3bd45edc9a29f7c0b70a30
Marc Muller <marc@muller.im>
2026-07-21 18:09:12 +0700

    fix(onboarding): echappements unicode explicites dans slugify (plan verbatim)

--- functions/api/_onboarding-lib.js
+++ functions/api/_onboarding-lib.js
@@ -12,7 +12,7 @@ export function slugify(s) {
     .toString()
     .toLowerCase()
     .normalize('NFD')
-    .replace(/[̀-ͯ]/g, '')
+    .replace(/[\u0300-\u036f]/g, '')
     .replace(/[^a-z0-9]+/g, '-')
     .replace(/-+/g, '-')
     .replace(/^-|-$/g, '');
